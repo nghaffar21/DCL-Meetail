d59121432ce38e156077869d19c18449c29bc2b4
mohammadreza <hamedi.mohamadreza@gmail.com>
2022-08-21 18:03:24 +0300

    polishing

--- src/Index.ts
+++ src/Index.ts
@@ -1,10 +1,10 @@
-import { LoadMainBuilding } from "./GameObjects/LoadBuilding";
+import { LoadMainBuilding } from "./GameObjects/Other/LoadBuilding";
 import { GroundPanel } from "./GameObjects/Panels/GroundFloorPanel";
 import { SecondFloorPanel } from "./GameObjects/Panels/SecondFloorPlanel";
 import { ThirdFloorPanel } from "./GameObjects/Panels/ThirdFloorPanel";
 import { FourthFloorPanel } from "./GameObjects/Panels/FourthFloorPanel";
 import { FifthFloorPanel } from "./GameObjects/Panels/FifthFloorPanel";
-import { HudBoxTest } from "./GameObjects/HudBox";
+import { HudBoxTest } from "./GameObjects/Other/HudBox";
 
 
 LoadMainBuilding();
